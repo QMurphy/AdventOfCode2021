11cc230226ea044bd36ccba93fc178e5e75b33bb
Quinn Murphy <QMurphy64@gmail.com>
2021-12-02 22:50:36 -0600

    Day 1 and Day 2 Comments

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,21 +1,29 @@
 use std::fs::File;
 use std::io::{ BufReader, BufRead };
 
+/// Parses an input file into a Vec of i32s
 fn get_input
     (
     path : &str
     ) -> Vec<i32>
 {
+    // Create return vector
     let mut v : Vec<i32> = Vec::new();
+
+    // Open the input, panic otherwise
     let file = match File::open( path )
     {
         Err( why ) => panic!( "Couldn't open {}: {}", path, why ),
         Ok( file ) => file
     };
+
+    // For each line
     for line in BufReader::new( file ).lines()
     {
+        // Push the i32 from the line into v, panic otherwise
         v.push( match line
         {
+            // Try to parse the string to an i32, panic otherwise
             Ok( s ) => match s.parse::<i32>()
             {
                 Ok( n ) => n,
@@ -31,8 +39,11 @@ pub fn part1() -> String
 {
     let mut num_increasing = 0;
     let depths = get_input( "../../res/day1/input.txt" );
+
+    // For each depth reading
     for i in 1..depths.len()
     {
+        // Increment num_increasing if it increased
         if( depths[i] > depths[i - 1] )
         {
             num_increasing += 1;
@@ -45,8 +56,11 @@ pub fn part2() -> String
 {
     let mut num_increasing = 0;
     let depths = get_input( "../../res/day1/input.txt" );
+
+    // For each depth reading
     for i in 3..depths.len()
     {
+        // Increment num_increasing if it increased with a moving window of 3
         if( depths[i] + depths[i - 1] + depths[i - 2] > depths[i - 1] + depths[i - 2] + depths[i - 3] )
         {
             num_increasing += 1;
--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{ BufReader, BufRead };
 
+/// A Movement enum with three variants that hold integers
 enum Movement
 {
     Forward( i32 ),
@@ -8,29 +9,41 @@ enum Movement
     Down( i32 )
 }
 
+/// Parses an input file into a Vec of Movements
 fn get_input
     (
     path : &str
     ) -> Vec<Movement>
 {
+    // Create return vector
     let mut v : Vec<Movement> = Vec::new();
+    
+    // Open the input, panic otherwise
     let file = match File::open( path )
     {
         Err( why ) => panic!( "Couldn't open {}: {}", path, why ),
         Ok( file ) => file
     };
+    
+    // For each line
     for line in BufReader::new( file ).lines()
     {
+        // Push the Movement from the line into v, panic otherwise
         v.push( match line
         {
             Ok( s ) =>
             {
+                // Split the string by spaces
                 let pair_vec = s.split( " " ).collect::<Vec<&str>>();
+
+                // Try to parse the second string to an i32, panic otherwise
                 let dist =  match pair_vec[1].parse::<i32>()
                 {
                     Ok( n ) => n,
                     Err( e ) => panic!( "Unable to parse distance: {}", e )
                 };
+
+                // Try to parse the first string to an enum variant, panic otherwise
                 match pair_vec[0]
                 {
                     "forward" => Movement::Forward( dist ),
@@ -50,8 +63,11 @@ pub fn part1() -> String
     let mut horizontal : i32 = 0;
     let mut depth : i32 = 0;
     let movement = get_input( "../../res/day2/input.txt" );
+
+    // For each movement
     for m in movement
     {
+        // Apply changes to variables
         match m
         {
             Movement::Forward( d ) => horizontal += d,
@@ -68,8 +84,11 @@ pub fn part2() -> String
     let mut depth = 0;
     let mut aim = 0;
     let movement = get_input( "../../res/day2/input.txt" );
+    
+    // For each movement
     for m in movement
     {
+        // Apply changes to variables
         match m
         {
             Movement::Forward( d ) =>
